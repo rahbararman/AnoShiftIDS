7a759973600ed5101782bcedee9b88ab3624efd4
rahbararman <rahbararman@gmail.com>
2023-08-01 20:58:45 +0200

    add workflow

---
+++ airflow/dags/workflows.py
@@ -0,0 +1,47 @@
+# airflow/dags/workflows.py
+from pathlib import Path
+
+from great_expectations_provider.operators.great_expectations import (
+    GreatExpectationsOperator,
+)
+
+from airflow.decorators import dag
+from airflow.operators.python_operator import PythonOperator
+from airflow.utils.dates import days_ago
+from config import config
+from IDSAnoShift import main
+
+GE_ROOT_DIR = Path(config.BASE_DIR, "tests", "great_expectations")
+
+# Default DAG args
+default_args = {
+    "owner": "airflow",
+    "catch_up": False,
+}
+
+
+@dag(
+    dag_id="DataValidationDag",
+    description="Validate Data",
+    default_args=default_args,
+    schedule_interval=None,
+    start_date=days_ago(2),
+    tags=["ML"],
+)
+def data_validation():
+    extract = PythonOperator(
+        task_id="extract",
+        python_callable=main.extract_data,
+        op_kwargs={},
+    )
+    validate = GreatExpectationsOperator(
+        task_id="validate",
+        checkpoint_name="traffic_data",
+        data_context_root_dir=GE_ROOT_DIR,
+        fail_task_on_validation_failure=True,
+    )
+    extract >> validate
+
+
+# Run DAG
+ml = data_validation()
